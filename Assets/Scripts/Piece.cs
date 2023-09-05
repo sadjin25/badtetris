b5132d6a90b825b6c6fc117c3e161c0d6de7b53a
sadjin25 <sadjin25@users.noreply.github.com>
2023-09-05 19:18:20 +0900

    feat: basic rotate(no wall kick, can stuck)

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -9,10 +9,20 @@ public class Piece : MonoBehaviour
     public Vector3Int position { get; private set; }
     public Vector3Int[] cells { get; private set; }
     public MinoData data { get; private set; }
+    public int rotateIndex { get; private set; }
 
     void Update()
     {
         board.Clear(this);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            Rotate(1);
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Move(Vector2Int.left);
@@ -48,12 +58,53 @@ public class Piece : MonoBehaviour
     {
         Vector3Int newPos = this.position + (Vector3Int)moveVec;
         Debug.Log("newpos" + newPos);
+
         bool valid = board.IsValidPosition(this, newPos);
         if (valid)
         {
             position = newPos;
         }
         return valid;
+    }
+
+    void Rotate(int rotateDir)
+    {
+        rotateIndex = Wrap(rotateIndex + rotateDir, 0, 3);
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+            int x = (int)cell.x, y = (int)cell.y;
+            switch (this.data.mino)
+            {
+                case Mino.O:
+                    break;
+                case Mino.I:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * rotateDir) + (cell.y * Data.RotationMatrix[1] * rotateDir));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * rotateDir) + (cell.y * Data.RotationMatrix[3] * rotateDir));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * rotateDir) + (cell.y * Data.RotationMatrix[1] * rotateDir));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * rotateDir) + (cell.y * Data.RotationMatrix[3] * rotateDir));
+                    break;
+            }
+            this.cells[i] = new Vector3Int(x, y, 0);
+
+        }
+    }
+
+    int Wrap(int target, int min, int max)
+    {
+        if (target < min)
+        {
+            return max;
+        }
+        if (target > max)
+        {
+            return min;
+        }
+        return target;
     }
 
 }
