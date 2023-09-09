6114a06c1ca4272085f44327e4503a5374564676
sadjin25 <sadjin25@users.noreply.github.com>
2023-09-10 08:35:24 +0900

    feat: ghost mino

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Ghost : MonoBehaviour
+{
+    [SerializeField] Tile tile;
+    [SerializeField] Board board;
+    [SerializeField] Piece activePiece;
+
+    public Tilemap tilemap { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    void Awake()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+        cells = new Vector3Int[4];
+    }
+
+    void LateUpdate()
+    {
+        Clear();
+        Copy();
+        Drop();
+        Set();
+    }
+
+    void Clear()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3Int tilePos = cells[i] + position;
+            this.tilemap.SetTile(tilePos, null);
+        }
+    }
+
+    void Copy()
+    {
+        for (int i = 0; i < cells.Length; ++i)
+        {
+            cells[i] = activePiece.cells[i];
+        }
+        this.position = activePiece.position;
+    }
+
+    void Drop()
+    {
+        Vector3Int pos = this.position;
+        int cur = pos.y;
+        int bottom = -board.bounds.height / 2 - 1;
+        board.Clear(activePiece);
+        for (int row = cur; row >= bottom; --row)
+        {
+            pos.y = row;
+            if (board.IsValidPosition(activePiece, pos))
+            {
+                this.position = pos;
+            }
+            else
+            {
+                break;
+            }
+        }
+        // original active piece is deleted, draw it again
+        board.Set(activePiece);
+    }
+
+    void Set()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3Int tilePos = cells[i] + position;
+            this.tilemap.SetTile(tilePos, tile);
+        }
+    }
+}
